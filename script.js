c158ff2bcf92828e04a7b43d3ca1f8a57bca8f5b
Cha-er-si <vpcharles31@gmail.com>
2024-08-08 15:02:19 +0800

    About Page Done

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
+/* Document on Load */
+document.addEventListener("DOMContentLoaded", (event) => {
+  const pathname = window.location.hash.replace("#", "");
+
+  navigationActiveState(pathname);
+
+  console.log("Pathname:", pathname);
+
+  toggleMenu(false);
+});
+
+/* Listen to URL changes */
+window.addEventListener("popstate", (event) => {
+  const pathname = window.location.hash.replace("#", "");
+
+  navigationActiveState(pathname);
+
+  console.log("Pathname:", pathname);
+
+  toggleMenu(false);
+});
+
 /* Menu */
-const toggleMenu = () => {
+const toggleMenu = (onCLick = true) => {
   const hamburger_menu = document.querySelector(".hamburger");
   const nav_list = document.querySelector(".nav-list ul");
 
   console.log({ hamburger_menu, nav_list });
 
-  hamburger_menu.classList.toggle("active");
-  nav_list.classList.toggle("active");
+  if (onCLick) {
+    hamburger_menu.classList.toggle("active");
+    nav_list.classList.toggle("active");
+  } else {
+    hamburger_menu.classList.remove("active");
+    nav_list.classList.remove("active");
+  }
+};
+
+const navigate = (event) => {
+  const target_element = event.target;
+  const target_element_name = target_element.innerText;
+
+  navigationActiveState(target_element_name);
+
+  toggleMenu(false);
+};
+
+const navigationActiveState = (value) => {
+  const sections = document.querySelectorAll("section:not(#navigation)");
+
+  sections.forEach((element) => {
+    console.log({ value: value.toLowerCase(), id: element.id });
+    if (element.id === value.toLowerCase()) {
+      element.classList.add("active");
+    } else if (value.toLowerCase() === "home" && element.id === "hero") {
+      element.classList.add("active");
+    } else {
+      element.classList.remove("active");
+    }
+  });
 };
