0aa6f495a6536050842ea83b674911b23dd532e7
Cha-er-si <princecharles.velarde@gmail.com>
2024-08-18 14:13:22 +0800

    Projects Section Done

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   console.log("Pathname:", pathname);
 
+  addProjectsToDOM();
+
   toggleMenu(false);
 });
 
@@ -24,15 +26,26 @@ window.addEventListener("popstate", (event) => {
 const toggleMenu = (onCLick = true) => {
   const hamburger_menu = document.querySelector(".hamburger");
   const nav_list = document.querySelector(".nav-list ul");
+  const sections = document.querySelectorAll("section:not(#navigation)");
 
   console.log({ hamburger_menu, nav_list });
 
   if (onCLick) {
     hamburger_menu.classList.toggle("active");
     nav_list.classList.toggle("active");
+
+    sections.forEach((element) => {
+      element.style.position = "relative";
+      element.style.zIndex = -1;
+    });
   } else {
     hamburger_menu.classList.remove("active");
     nav_list.classList.remove("active");
+
+    sections.forEach((element) => {
+      element.style.position = "static";
+      element.style.zIndex = 1;
+    });
   }
 };
 
@@ -52,10 +65,107 @@ const navigationActiveState = (value) => {
     console.log({ value: value.toLowerCase(), id: element.id });
     if (element.id === value.toLowerCase()) {
       element.classList.add("active");
-    } else if (value.toLowerCase() === "home" && element.id === "hero") {
+    } else if (
+      (value.toLowerCase() === "home" || value === "") &&
+      element.id === "hero"
+    ) {
       element.classList.add("active");
     } else {
       element.classList.remove("active");
     }
   });
+};
+
+/* Projects */
+const fetchProjects = async () => {
+  try {
+    const response = await fetch("./projects.json");
+    const data = await response.json();
+    console.log({ response, data });
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+const addProjectsToDOM = async () => {
+  const project = await fetchProjects();
+
+  const personal = project.personal;
+  if (personal) {
+    const personal_element = document.querySelector(".personal");
+    addItemToDiv(personal, personal_element);
+  }
+
+  const tutorial = project.tutorial;
+  if (tutorial) {
+    const tutorial_element = document.querySelector(".tutorial");
+    addItemToDiv(tutorial, tutorial_element);
+  }
+
+  const front_end_mentor = project.frontEndMentor;
+  if (front_end_mentor) {
+    const front_end_mentor_element =
+      document.querySelector(".front-end-mentor");
+    addItemToDiv(front_end_mentor, front_end_mentor_element);
+  }
+
+  const designs = project.designs;
+  if (designs) {
+    const designs_element = document.querySelector(".designs");
+    addItemToDiv(designs, designs_element);
+  }
+};
+
+const htmlToNode = (htmlString) => {
+  const create_div = document.createElement("div");
+  create_div.innerHTML = htmlString;
+  return create_div.firstChild;
+};
+
+const addItemToDiv = (json, elementDiv) => {
+  json.forEach((item) => {
+    const element = `<div class="project-items">
+          <h4 class="project-title">${item.title}</h4>
+          <p class="project-description">
+            ${item.description}
+          </p>
+          <div class="project-skills">
+            ${item.skills
+              .map((skill) => {
+                return `<p>${skill}</p>`;
+              })
+              .join()
+              .replace(/,/g, "")}
+          </div>
+        </div>`;
+
+    const element_to_node = htmlToNode(element);
+    elementDiv.appendChild(element_to_node);
+  });
+};
+
+const changeProjectDisplay = (event) => {
+  const project_links = document.querySelectorAll(".sub-links .sub-title");
+  const projects = document.querySelectorAll(".sub-container");
+
+  project_links.forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  projects.forEach((element) => {
+    element.classList.remove("active");
+    console.log(
+      event.target.innerHTML.toLowerCase(),
+      element.classList[0].toLowerCase()
+    );
+    if (
+      element.classList[0].toLowerCase().replace(/-/g, " ") ===
+      event.target.innerHTML.toLowerCase().replace(/-/g, " ")
+    ) {
+      element.classList.add("active");
+    }
+  });
+
+  event.target.classList.add("active");
 };
